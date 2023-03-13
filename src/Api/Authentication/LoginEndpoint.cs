c4654133526b0526e424d4982031281f1632bd35
Brijunas <32673235+Brijunas@users.noreply.github.com>
2023-03-13 09:34:32 +0200

    Basic login endpoint

---
+++ src/Api/Authentication/LoginEndpoint.cs
@@ -0,0 +1,14 @@
+﻿using MinimalApi.Endpoint;
+
+namespace Api.Authentication
+{
+    public class LoginEndpoint : IEndpoint
+    {
+        public void AddRoute(IEndpointRouteBuilder app)
+        {
+            app.MapPost("api/login", () => HandleAsync());
+        }
+
+        private Task<IResult> HandleAsync() => Task.FromResult(Results.Ok("Logged"));
+    }
+}
