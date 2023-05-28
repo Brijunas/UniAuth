28408ae6509523a0aeba67845dc4ea10cf4143a6
Brijunas <32673235+Brijunas@users.noreply.github.com>
2023-05-28 17:11:20 +0300

    Re-implemented minimalApi

---
+++ Api/Endpoints/WeatherForecast/Get.GetWeatherForecastResult.cs
@@ -0,0 +1,17 @@
+﻿namespace Api.Endpoints.WeatherForecast
+{
+    public class GetWeatherForecastResult
+    {
+        public GetWeatherForecastResult(DateOnly date, int temperatureC, string? summary)
+        {
+            Date = date;
+            TemperatureC = temperatureC;
+            Summary = summary;
+        }
+
+        public DateOnly Date { get; }
+        public int TemperatureC { get; }
+        public string? Summary { get; }
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
--- Api/Program.cs
+++ Api/Program.cs
@@ -1,5 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Catch all endpoints for swagger.
+builder.Services.AddControllers(options => options.UseNamespaceRouteToken());
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -16,29 +19,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+// Catch all endpoints.
+app.MapControllers();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
