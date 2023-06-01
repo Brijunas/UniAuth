fbeadc47033860d3cf1e897a6428e3467756d670
Brijunas <32673235+Brijunas@users.noreply.github.com>
2023-06-01 14:44:34 +0300

    Added index creation logic, to not forget

--- UniAuth.Infra/Repositories/UsernamesAuthRepository.cs
+++ UniAuth.Infra/Repositories/UsernamesAuthRepository.cs
@@ -1,4 +1,5 @@
-﻿using UniAuth.Domain.UsernamesAuth;
+﻿using MongoDB.Driver;
+using UniAuth.Domain.UsernamesAuth;
 using UniAuth.Infra.Database;
 
 namespace UniAuth.Infra.Repositories
@@ -9,7 +10,46 @@ namespace UniAuth.Infra.Repositories
         {
         }
 
-        public Task Create(UsernameAuth usernameAuth, CancellationToken cancellationToken = default) =>
-            collection.InsertOneAsync(usernameAuth, cancellationToken: cancellationToken);
+        public async Task Create(UsernameAuth usernameAuth, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                await collection.InsertOneAsync(usernameAuth, cancellationToken: cancellationToken);
+            }
+            catch (MongoWriteException e)
+            {
+                if (e.WriteError.Category == ServerErrorCategory.DuplicateKey)
+                    throw new Exception($"User {usernameAuth.Username} already exist.", e);
+
+                throw;
+            }
+        }
+
+        // Ensure index for username is unique.
+        // Need proper implementation for this.
+        public async Task EnsureIndexes()
+        {
+            const string indexName = "unique_Username";
+            var cursor = await collection.Indexes.ListAsync();
+            var indexes = await cursor.ToListAsync();
+
+            bool indexExists = false;
+            foreach (var index in indexes)
+            {
+                if (index["name"] == indexName)
+                {
+                    indexExists = true;
+                    break;
+                }
+            }
+
+            if (!indexExists)
+            {
+                var options = new CreateIndexOptions() { Unique = true };
+                var field = new StringFieldDefinition<UsernameAuth>(indexName);
+                var indexDefinition = new IndexKeysDefinitionBuilder<UsernameAuth>().Ascending(field);
+                await collection.Indexes.CreateOneAsync(new CreateIndexModel<UsernameAuth>(indexDefinition, options));
+            }
+        }
     }
 }
