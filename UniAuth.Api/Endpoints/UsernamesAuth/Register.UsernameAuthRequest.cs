acf821581a8d0f9bd5593da485e8b132fb66dd0d
Brijunas <32673235+Brijunas@users.noreply.github.com>
2023-06-01 20:47:52 +0300

    Added username validation

--- UniAuth.Api/Endpoints/UsernamesAuth/Register.UsernameAuthRequest.cs
+++ UniAuth.Api/Endpoints/UsernamesAuth/Register.UsernameAuthRequest.cs
@@ -1,9 +1,10 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Api.Endpoints.UsernamesAuth
 {
-    public class RegisterUsernameAuthRequest : UsernameAuthRequestBase, IValidatableObject
+    public partial class RegisterUsernameAuthRequest : UsernameAuthRequestBase, IValidatableObject
     {
         [Required]
         [MinLength(3)]
@@ -17,26 +18,38 @@ namespace Api.Endpoints.UsernamesAuth
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
-            static string errorMsg(string characterType) => $"Password must contain at least one {characterType}.";
+            // Username validation
+            if (Username.StartsWith(" ") || Username.EndsWith(" "))
+            {
+                yield return new ValidationResult("Username cannot start or end with whitespace", new[] { nameof(Username) });
+            }
+
+            if (!AplhanumericWithSeparators().IsMatch(Username))
+            {
+                yield return new ValidationResult("Username can only contain letters, numbers, and the characters _, -, and ., which must be separated by at least one letter or number", new[] { nameof(Username) });
+            }
+
+            // Password validation
+            static string passwordErrMsg(string characterType) => $"Password must contain at least one {characterType}.";
 
             if (!Password.Any(char.IsLower))
             {
-                yield return new ValidationResult(errorMsg("lowercase letter"), new[] { nameof(Password) });
+                yield return new ValidationResult(passwordErrMsg("lowercase letter"), new[] { nameof(Password) });
             }
 
             if (!Password.Any(char.IsUpper))
             {
-                yield return new ValidationResult(errorMsg("uppercase letter"), new[] { nameof(Password) });
+                yield return new ValidationResult(passwordErrMsg("uppercase letter"), new[] { nameof(Password) });
             }
 
             if (!Password.Any(char.IsDigit))
             {
-                yield return new ValidationResult(errorMsg("digit"), new[] { nameof(Password) });
+                yield return new ValidationResult(passwordErrMsg("digit"), new[] { nameof(Password) });
             }
 
             if (!Password.Any(ch => !char.IsLetterOrDigit(ch)))
             {
-                yield return new ValidationResult(errorMsg("non-alphanumeric character"), new[] { nameof(Password) });
+                yield return new ValidationResult(passwordErrMsg("non-alphanumeric character"), new[] { nameof(Password) });
             }
 
             // Calculate the size of the string in bytes
@@ -46,5 +59,8 @@ namespace Api.Endpoints.UsernamesAuth
                 yield return new ValidationResult("Password too long. It exceeds maximum byte limit of 56.", new[] { nameof(Password) });
             }
         }
+
+        [GeneratedRegex("^[a-zA-Z0-9]+([-_.][a-zA-Z0-9]+)*$")]
+        private static partial Regex AplhanumericWithSeparators();
     }
 }
